9ecfad87d31dc8f539dae6cf9256cadca68c7981
Sam Redmond <samredmondtech@gmail.com>
2019-01-27 16:43:55 +1300

    Intial Commit

---
+++ .gitignore/LastCard/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace LastCard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablePictureBox = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tablePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tablePictureBox
+            // 
+            this.tablePictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.tablePictureBox.BackColor = System.Drawing.Color.Green;
+            this.tablePictureBox.Location = new System.Drawing.Point(12, 14);
+            this.tablePictureBox.Name = "tablePictureBox";
+            this.tablePictureBox.Padding = new System.Windows.Forms.Padding(20);
+            this.tablePictureBox.Size = new System.Drawing.Size(988, 519);
+            this.tablePictureBox.TabIndex = 0;
+            this.tablePictureBox.TabStop = false;
+            this.tablePictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.tablePictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.tablePictureBox_MouseMove);
+            this.tablePictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.tablePictureBox_MouseUp);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 553);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1014, 619);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.tablePictureBox);
+            this.MinimumSize = new System.Drawing.Size(1030, 658);
+            this.Name = "Form1";
+            this.Text = "Last Card";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tablePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox tablePictureBox;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ .gitignore/LastCard/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LastCard
+{
+    public partial class Form1 : Form
+    {
+        Graphics cardTable;
+        Game game;
+        Card selectedCard;
+        bool mouseDown;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cardTable = tablePictureBox.CreateGraphics();
+            game = new Game(this);
+            
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            game.start();
+            game.MainDeck.Cards[0].flip();
+            game.DiscardPile.add(game.MainDeck.serve());
+            refresh();
+        }
+
+        private Card mouseOnCard(List<Card> pile, MouseEventArgs mousePos)
+        {
+            foreach (Card card in pile)
+            {
+                if (card.Area.Contains(mousePos.Location))
+                {
+                    return card;
+                }
+            }
+            return null;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            Card tempPlayerSelectedCard = mouseOnCard(game.PlayerHand.getCards(), e);
+            Card tempDeckSelectedCard = mouseOnCard(game.MainDeck.Cards, e);
+
+            if (tempPlayerSelectedCard != null)
+            {
+                selectedCard = tempPlayerSelectedCard;
+                selectedCard.OldX = selectedCard.X;
+                selectedCard.OldY = selectedCard.Y;
+            }
+            else if (tempDeckSelectedCard != null)
+            {
+                selectedCard = tempDeckSelectedCard;
+                selectedCard.OldX = selectedCard.X;
+                selectedCard.OldY = selectedCard.Y;
+            }
+
+
+        }
+
+        private void tablePictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown) { game.moveCard(selectedCard, e); }
+        }
+
+        private void tablePictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+            game.checkMatch(e, selectedCard);
+            selectedCard = null;
+        }
+
+        public void display(Card card)
+        {
+            cardTable.DrawImage(card.getImage(), card.X, card.Y, card.Width, card.Height);
+        }
+
+        public void coverCard(Card card)
+        {
+            cardTable.FillRectangle(new SolidBrush(Color.Green), new Rectangle(card.X, card.Y, card.Width, card.Height));
+        }
+
+        public void refresh()
+        {
+            drawCards(game.ComputerHand.getCards());
+            drawCards(game.PlayerHand.getCards());
+            drawCards(game.DiscardPile.Cards);
+            drawCards(new List<Card> { game.MainDeck.Cards[0] });
+        }
+
+        private void drawCards(List<Card> cards)
+        {
+            foreach (Card card in cards)
+            {
+                display(card);
+            }
+        }
+
+
+
+    }
+
+    
+}
